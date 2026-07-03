a8f6ec8f0644f002260e27c68d8ba5a8f904ea05
pauloalvesm <paulo.alves7351@gmail.com>
2026-07-03 20:07:47 -0300

    feat: apply logic to the production project

---
+++ src/KafkaProducer/Models/TransactionEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace KafkaProducer.Models;
+
+public class TransactionEvent
+{
+    public Guid Id { get; set; }
+    public string StoreName { get; set; } = string.Empty;
+    public decimal Amount { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
--- src/KafkaProducer/Program.cs
+++ src/KafkaProducer/Program.cs
@@ -1,7 +1,47 @@
-﻿internal class Program
+﻿using Confluent.Kafka;
+using KafkaProducer.Models;
+using System.Text.Json;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var config = new ProducerConfig
+        {
+            BootstrapServers = "localhost:9092"
+        };
+
+        using var producer = new ProducerBuilder<Null, string>(config).Build();
+        string topic = "transaction-topic";
+
+        Console.WriteLine($"--- Transaction Producer Started ({topic}) ---");
+        Console.WriteLine("Press Enter to send a new simulated transaction (or type 'exit' to quit):\n");
+
+        while (true)
+        {
+            string? command = Console.ReadLine();
+            if (command?.ToLower() == "exit") break;
+
+            var newTransaction = new TransactionEvent
+            {
+                Id = Guid.NewGuid(),
+                StoreName = "Central Tech Store",
+                Amount = new Random().Next(50, 1500),
+                CreatedAt = DateTime.UtcNow
+            };
+
+            string jsonPayload = JsonSerializer.Serialize(newTransaction);
+
+            try
+            {
+                var result = await producer.ProduceAsync(topic, new Message<Null, string> { Value = jsonPayload });
+
+                Console.WriteLine($"[Success] Transaction {newTransaction.Id} sent! Amount: ${newTransaction.Amount:N2}");
+            }
+            catch (ProduceException<Null, string> e)
+            {
+                Console.WriteLine($"[Error] Failed to send message: {e.Error.Reason}");
+            }
+        }
     }
 }
